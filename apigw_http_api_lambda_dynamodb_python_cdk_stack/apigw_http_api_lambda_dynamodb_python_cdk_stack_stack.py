711c0706b4c914792805b48d19cbfdb5c727449c
adarsh maharjan <a-maharjan@ryowahouse.co.jp>
2026-04-01 11:28:38 +0900

    feat: created cdk stack

--- apigw_http_api_lambda_dynamodb_python_cdk_stack/apigw_http_api_lambda_dynamodb_python_cdk_stack_stack.py
+++ apigw_http_api_lambda_dynamodb_python_cdk_stack/apigw_http_api_lambda_dynamodb_python_cdk_stack_stack.py
@@ -1,9 +1,16 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from typing import cast, Sequence
 from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_dynamodb as dynamodb_,
+    aws_lambda as lambda_,
+    aws_apigateway as apigw_,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    Duration,
+)
+
+TABLE_NAME = "demo_table"
 
 class ApigwHttpApiLambdaDynamodbPythonCdkStackStack(Stack):
 
@@ -17,3 +24,76 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStackStack(Stack):
         #     self, "ApigwHttpApiLambdaDynamodbPythonCdkStackQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        vpc = ec2.Vpc(
+            self,
+            id = "Ingress",
+            cidr = "10.0.0.0/16",
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Private Subnet",
+                    subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidr_mask = 24
+                )
+            ]
+        )
+
+        dynamo_db_endpoint = ec2.GatewayVpcEndpoint(
+            self,
+            id="DynamoDBVpc",
+            service=ec2.GatewayVpcEndpointAwsService.DYNAMODB,
+            vpc = vpc
+        )
+
+        dynamo_db_endpoint.add_to_policy(
+            iam.PolicyStatement(
+                principals=cast(Sequence[iam.IPrincipal], [iam.AnyPrincipal()]),
+                actions=[
+                    "dynamodb:DescribeStream",
+                    "dynamodb:DescribeTable",
+                    "dynamodb:Get*",
+                    "dynamodb:Query",
+                    "dynamodb:Scan",
+                    "dynamodb:CreateTable",
+                    "dynamodb:Delete*",
+                    "dynamodb:Update*",
+                    "dynamodb:PutItem",
+                ],
+                resources = ["*"]
+            )
+        )
+
+        demo_table = dynamodb_.Table(
+            self,
+            TABLE_NAME,
+            partition_key = dynamodb_.Attribute(
+                name="id", type=dynamodb_.AttributeType.STRING,
+            )
+        )
+
+        api_handler = lambda_.Function(
+            self,
+            "ApiHandler",
+            function_name = "apigw_handler",
+            runtime= lambda_.Runtime.PYTHON_3_9,
+            code = lambda_.Code.from_asset("lambda/apigw-handler"),
+            handler = "index.handler",
+            vpc = vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+            ),
+            memory_size = 1024,
+            timeout=Duration.minutes(5),
+        )
+
+        demo_table.grant_write_data(api_handler)
+        api_handler.add_environment("TABLE_NAME", demo_table.table_name)
+
+        apigw_.LambdaRestApi(
+            self,
+            "Endpoint",
+            handler=cast(lambda_.IFunction, api_handler)
+        )
+
+
+
